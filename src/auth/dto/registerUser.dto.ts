7311a9ce2ed966de98fbe477f7e2cff2349c27e3
vishal Maurya <vishal@aerohub.aero>
2025-12-09 17:51:56 +0530

    add jwt and validation

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,29 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
 export class RegisterDto {
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsString()
+    @Matches(/^\S*$/, {
+        message: 'first name should not contain spaces',
+    })
     fname: string;
+
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsString()
+    @Matches(/^\S*$/, {
+        message: 'last name should not contain spaces',
+    })
     lname: string;
+
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsString()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    @IsString()
     password: string;
 }
